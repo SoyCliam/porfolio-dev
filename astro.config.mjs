1c9f3663250b17af86cb313a371b21e74c655608
CliamRose <cliamrose@gmail.com>
2025-06-10 18:21:03 -0500

    Deshabilitar Jekyll en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   base: '/porfolio-dev/',
-  // reemplaza con el nombre exacto de tu repo
   integrations: [tailwind()],
 });
